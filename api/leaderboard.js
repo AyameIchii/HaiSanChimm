173625b928b7b2f4ee905f44cb6d2c40b987207d
AyameIchii <157225128+AyameIchii@users.noreply.github.com>
2025-07-31 10:49:07 +0700

    Update leaderboard.js

--- api/leaderboard.js
+++ api/leaderboard.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = 'https://YOUR_PROJECT_ID.supabase.co'
-const SUPABASE_KEY = 'YOUR_ANON_KEY'
+const SUPABASE_URL = 'https://zwixilhhgrwhftnhgifh.supabase.co'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp3aXhpbGhoZ3J3aGZ0bmhnaWZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM5MzI0MzMsImV4cCI6MjA2OTUwODQzM30.3pE8bDG67aqi3gFEG-JQu31zsJynVElFbfhOsLgFAO0'
 
 async function saveScore(name, score) {
   const res = await fetch(`${SUPABASE_URL}/rest/v1/scores`, {
